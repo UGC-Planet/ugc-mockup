fe7bfca58620564941a2f3f60b268bb5d73c48b2
Sebastian hilton <bastianh86@outlook.com>
2024-07-03 18:53:20 +0100

    started project structure and uploaded to netlify for visual inspection of current work

--- pages/Search/results.vue
+++ pages/Search/results.vue
@@ -10,19 +10,11 @@
 </template>
 
 <script>
-    import {
-        instantMeiliSearch
-    } from "@meilisearch/instant-meilisearch";
-    import "instantsearch.css/themes/algolia-min.css";
-
 
     export default {
         data() {
             return {
-                searchClient: instantMeiliSearch(
-                    "https://integration-demos.meilisearch.com",
-                    "99d1e034ed32eb569f9edc27962cccf90b736e4c5a70f7f5e76b9fab54d6a185"
-                ),
+
             };
         },
     };
